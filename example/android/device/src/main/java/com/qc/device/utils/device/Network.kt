e26a5dc9b40f2ea45c4666094bc2fc03374e909f
atlands <atlands@qq.com>
2023-07-31 17:04:12 +0800

    移除module device，使用dependencies引入

--- example/android/device/src/main/java/com/qc/device/utils/device/Network.kt
+++
@@ -1,8 +0,0 @@
-package com.qc.device.utils.device
-
-import com.qc.device.model.Device
-import com.qc.device.utils.DeviceUtil
-
-fun DeviceUtil.getNetwork(): Device.Network {
-    return Device.Network()
-}
